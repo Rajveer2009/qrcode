2a6364dd1611f7248faaa567fe55abd990a3c02f
Rajveer Singh Saggu <93126793+Rajveer2009@users.noreply.github.com>
2023-04-18 20:51:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import qrcode
+import os
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route('/<data>', methods=['GET', 'POST'])
+def main(data):
+    img = qrcode.make(data)
+    img_path = os.path.join('static', f'{data}.png')
+    img.save(img_path)
+    try:
+        return send_file(img_path, as_attachment=True)
+    finally:
+        os.remove(img_path)
+
+if __name__ == '__main__':
+    app.run()
